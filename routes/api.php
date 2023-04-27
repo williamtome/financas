83a873678957e3a728528ecfae89c41192f7113c
William Tomé <william.tomenh@gmail.com>
2023-04-27 16:09:21 -0300

    fix(routes): change route to get authenticated user

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')
-    ->get('/', function (Request $request) {
+    ->get('/user', function (Request $request) {
         dd('Usuário autenticado:',$request->user());
     });
 
