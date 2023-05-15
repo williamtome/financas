bb1c62cc94a6999796f9fb03e7d3ee32a2229365
William Tomé <william.tomenh@gmail.com>
2023-05-15 09:25:21 -0300

    test(UserRepositoryTest) add attribute to get repository and use in all life cycle tests.

--- tests/Feature/App/Repository/Eloquent/UserRepositoryTest.php
+++ tests/Feature/App/Repository/Eloquent/UserRepositoryTest.php
@@ -5,21 +5,30 @@ namespace Tests\Feature\App\Repository\Eloquent;
 use App\Models\User;
 use App\Repository\Contracts\UserRepositoryInterface;
 use App\Repository\Eloquent\UserRepository;
+use Illuminate\Database\QueryException;
 use Tests\TestCase;
 
 class UserRepositoryTest extends TestCase
 {
+    private UserRepository $repository;
+
+    protected function setUp(): void
+    {
+        $this->repository = new UserRepository(new User());
+
+        parent::setUp();
+    }
+
     public function test_implements_interface(): void
     {
         $this->assertInstanceOf(
             UserRepositoryInterface::class,
-            new UserRepository(new User())
+            $this->repository
         );
     }
     public function test_find_all_empty(): void
     {
-        $repository = new UserRepository(new User());
-        $response = $repository->findAll();
+        $response = $this->repository->findAll();
 
         $this->assertIsArray($response);
         $this->assertCount(0, $response);
@@ -29,9 +38,37 @@ class UserRepositoryTest extends TestCase
     {
         User::factory()->count(10)->create();
 
-        $repository = new UserRepository(new User());
-        $response = $repository->findAll();
+        $response = $this->repository->findAll();
 
         $this->assertCount(10, $response);
+    }
+
+    public function test_create()
+    {
+        $data = [
+            'name' => 'William Tomé',
+            'email' => 'william@teste.com',
+            'password' => bcrypt('12345678'),
+        ];
+
+        $response = $this->repository->create($data);
+
+        $this->assertNotNull($response);
+        $this->assertIsObject($response);
+        $this->assertDatabaseHas('users', [
+            'email' => 'william@teste.com',
+        ]);
+    }
+
+    public function test_throw_exception()
+    {
+        $this->expectException(QueryException::class);
+
+        $data = [
+            'name' => 'William Tomé',
+            'password' => bcrypt('12345678'),
+        ];
+
+        $this->repository->create($data);
     }
 }
