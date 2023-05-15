5af6afc817840acb89871b1176ed4a400f16aa77
William Tomé <william.tomenh@gmail.com>
2023-05-15 09:56:56 -0300

    feat(UserRepository) update user.

--- app/Repository/Contracts/UserRepositoryInterface.php
+++ app/Repository/Contracts/UserRepositoryInterface.php
@@ -6,4 +6,5 @@ interface UserRepositoryInterface
 {
     public function findAll(): array;
     public function create(array $data): object;
+    public function update(string $email, array $data): object;
 }
