2f1e8d61a158768e000a08d7e00351050a0e6a38
William Tomé <william.tomenh@gmail.com>
2023-05-15 08:43:54 -0300

    feat(UserRepository) add more tests for user repository.

---
+++ app/Repository/Contracts/UserRepositoryInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Repository\Contracts;
+
+interface UserRepositoryInterface
+{
+    public function findAll(): array;
+}
--- tests/Feature/App/Repository/Eloquent/UserRepositoryTest.php
+++ tests/Feature/App/Repository/Eloquent/UserRepositoryTest.php
@@ -3,21 +3,35 @@
 namespace Tests\Feature\App\Repository\Eloquent;
 
 use App\Models\User;
+use App\Repository\Contracts\UserRepositoryInterface;
 use App\Repository\Eloquent\UserRepository;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UserRepositoryTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    public function test_implements_interface(): void
+    {
+        $this->assertInstanceOf(
+            UserRepositoryInterface::class,
+            new UserRepository(new User())
+        );
+    }
+    public function test_find_all_empty(): void
     {
         $repository = new UserRepository(new User());
         $response = $repository->findAll();
 
         $this->assertIsArray($response);
+        $this->assertCount(0, $response);
+    }
+
+    public function test_find_all(): void
+    {
+        User::factory()->count(10)->create();
+
+        $repository = new UserRepository(new User());
+        $response = $repository->findAll();
+
+        $this->assertCount(10, $response);
     }
 }
