8e5dc29b854f3b802f1e765a50bf921b3aff0cae
William Tomé <william.tomenh@gmail.com>
2023-05-17 11:16:09 -0300

    style(api/users): move User controller namespace on top file.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', [\App\Http\Controllers\Api\UserController::class, 'index']);
+Route::get('/users', [UserController::class, 'index']);
+
