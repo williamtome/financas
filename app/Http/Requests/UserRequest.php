2a72e8c7336c22a836437cab7a81bd0620540bbe
William Tomé <william.tomenh@gmail.com>
2023-05-25 23:21:20 -0300

    feat(UserController): change rules when http verb is put.

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -13,10 +13,18 @@ class UserRequest extends FormRequest
 
     public function rules(): array
     {
-        return [
+        $rules = [
             'name' => 'required|string|max:255',
-            'email' => 'required|email|max:255',
+            'email' => 'required|email|max:255|unique:users,email',
             'password' => 'required|min:6|max:15',
         ];
+
+        if ($this->method() === 'PUT') {
+            $rules['email'] = 'sometimes|email|max:255';
+            $rules['password'] = 'sometimes|min:6|max:15';
+            return $rules;
+        }
+
+        return $rules;
     }
 }
