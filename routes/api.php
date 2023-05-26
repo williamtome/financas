5405a2cd2593fd66eb48e460fb2f047ae9812d4b
William Tomé <william.tomenh@gmail.com>
2023-05-25 23:48:09 -0300

    refactor(routes/api): use api resource for user routes.

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,6 @@
 <?php
 
 use App\Http\Controllers\Api\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', [UserController::class, 'index']);
-Route::post('/users', [UserController::class, 'store']);
-Route::get('/users/{email}', [UserController::class, 'show']);
-Route::put('/users/{email}', [UserController::class, 'update']);
+Route::apiResource('/users', UserController::class);
